f50c7a1651e576c9305bc7aef7ac750ab517220e
Your Name <brianmwangi10808@gmail.com>
2024-01-31 08:37:11 +0300

    uncompleted

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// your_script.js
 
 const apiUrl2 = "/db.json";
 
@@ -7,22 +6,30 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(apiUrl2)
         .then(response => response.json())
         .then(movieList => {
-            if (movieList && movieList.length > 0) {
-                const movieData = movieList[0];
-
-                // Calculate available tickets
-                const availableTickets = movieData.capacity - movieData.tickets_sold;
-
-                // Update HTML content
+            if (movieList && movieList.films.length > 0) {
                 const movieDetailsContainer = document.getElementById('user-list');
-                movieDetailsContainer.innerHTML = `
-                    <img src="${movieData.poster}" alt="${movieData.title} Poster" style="max-width: 100%">
-                    <h2>${movieData.title}</h2>
-                    <p>Runtime: ${movieData.runtime} minutes</p>
-                    <p>Showtime: ${movieData.showtime}</p>
-                    <p>Available Tickets: ${availableTickets}</p>
-                    <p>${movieData.description}</p>
-                `;
+
+                for (let i = 0; i < movieList.films.length; i++) {
+                    const movieData = movieList.films[i];
+
+                    // Calculate available tickets
+                    const availableTickets = movieData.capacity - movieData.tickets_sold;
+
+                    // Create HTML content
+                    const movieDetails= `
+                        <div>
+                            <img src="${movieData.poster}" alt="${movieData.title} Poster" style="max-width: 100%">
+                            <h2>${movieData.title}</h2>
+                            <p>Runtime: ${movieData.runtime} minutes</p>
+                            <p>Showtime: ${movieData.showtime}</p>
+                            <p>Available Tickets: ${availableTickets}</p>
+                            <p>${movieData.description}</p>
+                        </div>
+                    `;
+
+                    // Add movie details to container
+                    movieDetailsContainer.innerHTML += movieDetails;
+                }
             } else {
                 console.error('Movie data not available.');
             }
